3754b7a52bdc564f774e85a058adfaaae031ab5b
Mohit <mohitshaw181104@gmail.com>
2023-10-25 14:35:39 +0530

    updated file 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const App = () => {
+export default function App() {
   return (
-    <div></div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
