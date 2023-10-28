5c3d70b98519113cbada2c7353e40996f90aa139
Mohit <mohitshaw181104@gmail.com>
2023-10-28 14:07:31 +0530

    ..

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -12,9 +12,9 @@ const Features = () => {
       <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" className="border-black">
       </path>
     </svg>
-  </button>
+     </button>
 
-</div>
+    </div>
   
   );
 };
