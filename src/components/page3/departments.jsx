6498364ec25044b6275131ece9eb97a22c4577f1
aditya22arya <124781262+aditya22arya@users.noreply.github.com>
2023-10-31 20:53:21 +0530

    Add files via upload
    added more icons and finished the department section

--- src/components/page3/departments.jsx
+++ src/components/page3/departments.jsx
@@ -3,22 +3,22 @@ import'./departments.css'
 const departments = () => {
   return (
     <div className='department'>
-        <h1>list of departments</h1>
+        <h1>List of Departments</h1>
         <div className='departments_1'>
-            <button><img src="src\assets\DefenceIcon.png" alt="" /></button>
-            <button><img src="src\assets\EducationIcon.png" alt="" /></button>
-            <button><img src="src\assets\Finance.png" alt="" /></button>
-            <button><img src="src\assets\Gov_sector.png" alt="" /></button>
-            <button><img src="src\assets\HealthIcon.png" alt="" /></button>
-            <button><img src="src\assets\Identity_icon.png" alt="" /></button>
-            <button><img src="src\assets\Industry_sector.png" alt="" /></button>
-            <button><img src="src\assets\Transport.png" alt="" /></button>
-            <button>9</button>
-            <button>10</button>
-            <button>11</button>
-            <button>12</button>
-            <button>13</button>
-            <button><img src="src\assets\others.png" alt="" /></button>
+            <button><img src="src\assets\DefenceIcon.png" alt="" />defence</button>
+            <button><img src="src\assets\EducationIcon.png" alt="" />Education</button>
+            <button><img src="src\assets\Finance.png" alt="" />Finance</button>
+            <button><img src="src\assets\Gov_sector.png" alt="" />Gov sector</button>
+            <button><img src="src\assets\HealthIcon.png" alt="" />Health</button>
+            <button><img src="src\assets\Identity_icon.png" alt="" />Cybersecurity</button>
+            <button><img src="src\assets\Industry_sector.png" alt="" />Industry</button>
+            <button><img src="src\assets\Transport.png" alt="" />Transport</button>
+            <button><img src="src\assets\agri.png" alt="" />Agriculture</button>
+            <button><img src="src\assets\envi.png" alt="" />Environment</button>
+            <button><img src="src\assets\family.png" alt="" />Insurance</button>
+            <button><img src="src\assets\ecommerce.png" alt="" />E-commerce</button>
+            <button><img src="src\assets\water.png" alt="" />Water</button>
+            <button><img src="src\assets\others.png" alt=""/>Others</button>
         </div>            
     </div>
   )
